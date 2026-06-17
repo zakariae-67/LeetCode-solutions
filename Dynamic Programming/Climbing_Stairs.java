fb8e1d2f569297deffc9a420eb1b3b74ab2e58e4
M.Zakariae <zaka.moussaoui@edu.umi.ac.ma>
2026-06-17 17:21:12 +0100

    Climbing Stairs solution

---
+++ Dynamic Programming/Climbing_Stairs.java
@@ -0,0 +1,22 @@
+package train;
+
+public class Climbing_Stairs {
+	
+	public int climbStairs(int n) {
+
+        if(n<=3) return n;
+
+        int prev2=2;
+        int prev3=3;
+        int curr=0;
+
+        for(int i=4;i<=n;i++){
+            curr=prev2+prev3;
+            prev2=prev3;
+            prev3=curr;
+        }
+
+        return curr;
+    }
+
+}
